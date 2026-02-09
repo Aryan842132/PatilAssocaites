2121767b0c1103025a25412b5b84b1ee35f25b73
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-02-09 16:24:00 +0530

    GET ALL

--- src/main/java/com/barAndRestaurants/controller/AuthController.java
+++ src/main/java/com/barAndRestaurants/controller/AuthController.java
@@ -1,26 +1,37 @@
 package com.barAndRestaurants.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.barAndRestaurants.model.AppUser;
 import com.barAndRestaurants.payload.LoginRequest;
 import com.barAndRestaurants.payload.SignupRequest;
 import com.barAndRestaurants.service.AuthService;
+import com.barAndRestaurants.repository.AppUserRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final AppUserRepository userRepository;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, AppUserRepository userRepository) {
         this.authService = authService;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<AppUser>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
     }
 
     @PostMapping("/login")
