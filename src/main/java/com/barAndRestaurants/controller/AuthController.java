40ee0c03dd3c91f8405833c0d5362226e037fc7c
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-02-09 13:05:16 +0530

    AUTH

--- src/main/java/com/barAndRestaurants/controller/AuthController.java
+++ src/main/java/com/barAndRestaurants/controller/AuthController.java
@@ -1,12 +1,17 @@
 package com.barAndRestaurants.controller;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.barAndRestaurants.payload.LoginRequest;
 import com.barAndRestaurants.payload.SignupRequest;
 import com.barAndRestaurants.service.AuthService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
